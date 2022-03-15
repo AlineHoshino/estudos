972ff5c012edea01cf9ce297aa37af17a4f22f25
ALINE <hoshinoaline@yahoo.com.br>
2022-03-14 21:14:32 -0300

    feat: add icons to video

---
+++ avanc_html/video/script.js
@@ -0,0 +1,33 @@
+{/* <a href="https://www.flaticon.com/br/icones-gratis/google-play-music" title="google play music ícones">Google play music ícones criados por IYAHICON - Flaticon</a>
+// vídeo for your deligth
+//<a href="https://www.flaticon.com/free-icons/rewind" title="rewind icons">Rewind icons created by Freepik - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/stop-button" title="stop button icons">Stop button icons created by joalfa - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/next" title="next icons">Next icons created by Roundicons - Flaticon</a> */}
+{/* <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by Chanut - Flaticon</a>
+<a href="https://www.flaticon.com/free-icons/minimize" title="minimize icons">Minimize icons created by Bingge Liu - Flaticon</a> */}
+
+let video = document.getElementById('video1');
+
+function retorceder(){
+
+}
+
+function avancar(){
+
+}
+
+function diminuir_vel(){
+
+}
+
+function aumentar(){
+
+}
+
+function play(){
+
+}
+
+function stop(){
+  
+}
