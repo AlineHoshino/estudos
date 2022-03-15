09d58d64f5ba150de048a51f4c810123a308632e
ALINE <hoshinoaline@yahoo.com.br>
2022-03-14 21:43:44 -0300

    feat: change video for one with 10mb

--- avanc_html/video/script.js
+++ avanc_html/video/script.js
@@ -5,29 +5,42 @@
 <a href="https://www.flaticon.com/free-icons/next" title="next icons">Next icons created by Roundicons - Flaticon</a> */}
 {/* <a href="https://www.flaticon.com/free-icons/plus" title="plus icons">Plus icons created by Chanut - Flaticon</a>
 <a href="https://www.flaticon.com/free-icons/minimize" title="minimize icons">Minimize icons created by Bingge Liu - Flaticon</a> */}
+// Imagens de flatIcon com referências
+// Vídeo de for your delight;
 
 let video = document.getElementById('video1');
 
-function retorceder(){
-
+function retroceder(){
+  video.currentTime -= 15;// próprio vídeo menos 15
 }
 
 function avancar(){
-
+  video.currentTime +=15;
 }
 
 function diminuir_vel(){
-
+  video.playbackRate -= 0.1;
 }
 
-function aumentar(){
-
+function aumentar_vel(){
+  video.playbackRate += 0.1;
 }
 
 function play(){
-
+  video.play();
 }
 
 function stop(){
-  
+  video.pause();
+  video.currentTime = 0;
+}
+
+let controles = document.getElementsByClassName('controle')[0];
+
+function show(){
+  controles.style.opacity="1";
+}
+
+function hide(){
+  controles.style.opacity="0";
 }
