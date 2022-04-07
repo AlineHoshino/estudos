2d4374d8a2a2e308416c5808db0000406fed6121
ALINE <hoshinoaline@yahoo.com.br>
2022-04-07 19:34:30 -0300

    feat: logica

--- logica/logica.js
+++ logica/logica.js
@@ -36,3 +36,16 @@ function isAgeDiverse(list7){
   }
   return true;
 }
+
+ function find_max(nums) {
+  let max_num = Number.NEGATIVE_INFINITY; // smaller than all other numbers
+   for (let num of nums) {
+   if (num > max_num) {
+    max_num = num
+   }
+   }
+   return max_num;
+   }
+  
+
+   console.log(find_max([1,2,3,4,5]))
